1240a5d5268f3e3a4e941e352b6b053ffc25b118
Noreh AD <akinoreh@gmail.com>
2023-09-05 21:41:49 +0300

    php: blank dashboard ui

---
+++ php/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-200 text-slate-700 [--header-height:4rem] [--aside-width:300px]">
+    
+    <header class="bg-[hsl(215,50%,98%)] h-[var(--header-height)] ml-[var(--aside-width)] shadow"></header>
+    
+    <aside class="bg-[hsl(215,50%,99%)] w-[var(--aside-width)] fixed h-[100vh] left-0 top-0 shadow z-10"></aside>
+    
+    <main class="ml-[var(--aside-width)] mt-4 p-4 min-h-[20vh] mb-[20vh]"></main>
+    
+</body>
+</html>
