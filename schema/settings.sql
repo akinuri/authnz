e1c18532fc5b7b86c88784995c0fc0e0e251873d
Noreh AD <akinoreh@gmail.com>
2023-09-05 21:01:21 +0300

    minor table changes

--- schema/settings.sql
+++ schema/settings.sql
@@ -1,9 +1,9 @@
 -- See https://en.wikipedia.org/wiki/Entity%E2%80%93attribute%E2%80%93value_model
 CREATE TABLE `settings` (
-    `id`         int(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id`         int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `entity`     varchar(255) DEFAULT NULL,
     `attribute`  varchar(255) NOT NULL,
     `value`      text DEFAULT NULL,
     `updated_at` datetime DEFAULT NULL,
-    `updated_by` int(10) UNSIGNED DEFAULT NULL
+    `updated_by` int UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- schema/users.sql
+++ schema/users.sql
@@ -1,15 +1,14 @@
 CREATE TABLE `users` (
-    `id`               int(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id`               int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email`            varchar(255) NOT NULL UNIQUE KEY,
     `password`         varchar(255) NOT NULL,
     `display_name`     varchar(255) NOT NULL,
-    `avatar`           varchar(255) DEFAULT NULL,
     `disabled_at`      DATETIME DEFAULT NULL,
     `loggedin_at`      DATETIME DEFAULT NULL,
     `created_at`       datetime NOT NULL,
-    `created_by`       int(10) UNSIGNED NOT NULL,
+    `created_by`       int UNSIGNED NOT NULL,
     `updated_at`       datetime DEFAULT NULL, 
-    `updated_by`       (10) UNSIGNED DEFAULT NULL
+    `updated_by`       int UNSIGNED DEFAULT NULL
     `authz_updated_at` DATETIME DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
