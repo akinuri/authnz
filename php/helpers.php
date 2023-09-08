87da932a7ba524ad3fe223e9fd37da7f0aac00bc
Noreh AD <akinoreh@gmail.com>
2023-09-08 23:53:48 +0300

    ui component()

--- php/helpers.php
+++ php/helpers.php
@@ -21,3 +21,30 @@ function htmlClass(): string {
     return implode(" ", func_get_args());
 }
 
+function joinPaths(array $paths, string $seperator = DIRECTORY_SEPARATOR) {
+    $paths = array_filter($paths, function ($path) {
+        return is_string($path) && strlen($path);
+    });
+    $paths = join($seperator, $paths);
+    $pattern = "/(\\\\\/)+|\\\+|\/+/";
+    $paths = preg_replace($pattern, $seperator, $paths);
+    return $paths;
+}
+
+define("COMPONENTS_PATH", __DIR__ . "/components");
+
+function component(string $componentName, array $data = []) {
+    $componentPath = realpath(joinPaths([COMPONENTS_PATH, "$componentName.php"]));
+    if (!file_exists($componentPath)) {
+        throw new Exception("Component ($componentName) not found.");
+    }
+    extract($data);
+    ob_start();
+    $isIncluded = include $componentPath;
+    $output = ob_get_clean();
+    if ($isIncluded === 1) {
+        return $output;
+    }
+    return $isIncluded;
+}
+
