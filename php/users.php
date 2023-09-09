436dcc9508eaac1c74192a32f8e9a15358c50d9d
Noreh AD <akinoreh@gmail.com>
2023-09-09 17:10:27 +0300

    dashboard layout in users page

--- php/users.php
+++ php/users.php
@@ -1,41 +1,27 @@
-<?php require_once "helpers.php" ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="bg-slate-200 text-slate-700 [--header-height:4rem] [--aside-width:300px]">
-    
-    <?php include "partials/header.php" ?>
-    
-    <?php include "partials/aside.php" ?>
-    
-    <main class="ml-[var(--aside-width)] p-6 min-h-[20vh] mb-[20vh]">
-        
-        <div class="container mx-auto">
-            
-            <?= component("content-header", [
+<?php
+    $GLOBALS["executionStartedAt"] = microtime(true);
+    require_once "helpers.php";
+    ob_start_onshutdown(function () {
+        echo layout(
+            "dashboard",
+            [
                 "title" => "Users",
-                "breadcrumbs" => [
-                    ["users.php", "Users"],
-                ],
-                "actions" => [
-                    ["add-user.php", "Add User"],
-                ],
-            ]) ?>
-            
-            <div class="bg-white rounded shadow h-96 p-4">
-                
-            </div>
-            
-        </div>
-        
-    </main>
+            ],
+            ob_get_clean(),
+        );
+    });
+?>
+
+<?= component("content-header", [
+    "title" => "Users",
+    "breadcrumbs" => [
+        ["users.php", "Users"],
+    ],
+    "actions" => [
+        ["add-user.php", "Add User"],
+    ],
+]) ?>
+
+<div class="bg-white rounded shadow h-96 p-4">
     
-    <script src="assets/global.js"></script>
-    
-</body>
-</html>
+</div>
