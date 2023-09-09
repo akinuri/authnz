8a562e0230043f761fee3c436cbbeb72be6c4d1b
Noreh AD <akinoreh@gmail.com>
2023-09-09 17:34:25 +0300

    -container in content-header

--- php/components/content-header.php
+++ php/components/content-header.php
@@ -1,31 +1,27 @@
-<section class="mb-3">
+<section class="mb-3 px-1 flex justify-between items-center">
     
-    <div class="container mx-auto px-1 flex justify-between items-center">
-        
-        <div>
-            <nav class="text-xs">
-                <ul class="flex gap-2">
-                    <?php for ($i = 0; $i < count($breadcrumbs); $i++) {
-                        $breadcrumb = $breadcrumbs[$i] ?>
-                    <li>
-                        <a class="text-blue-700 hover:underline" href="<?= $breadcrumb[0] ?>"><?= $breadcrumb[1] ?></a>
-                    </li>
-                    <?php if ($i != count($breadcrumbs) - 1) { echo "<li>/</li>"; } ?>
-                    <?php } ?>
-                </ul>
-            </nav>
-            <h1 class="text-lg font-bold"><?= $title ?? "Untitled Page" ?></h1>
-        </div>
-        
-        <div>
-            <?php foreach ($actions ?? [] as $action) { ?>
-            <a
-                class="text-sm px-3 py-2 rounded bg-blue-300 hover:bg-blue-400/70 active:bg-blue-400/90"
-                href="<?= $action[0] ?>"
-                ><?= $action[1] ?></a>
-            <?php } ?>
-        </div>
-        
+    <div>
+        <nav class="text-xs">
+            <ul class="flex gap-2">
+                <?php for ($i = 0; $i < count($breadcrumbs); $i++) {
+                    $breadcrumb = $breadcrumbs[$i] ?>
+                <li>
+                    <a class="text-blue-700 hover:underline" href="<?= $breadcrumb[0] ?>"><?= $breadcrumb[1] ?></a>
+                </li>
+                <?php if ($i != count($breadcrumbs) - 1) { echo "<li>/</li>"; } ?>
+                <?php } ?>
+            </ul>
+        </nav>
+        <h1 class="text-lg font-bold"><?= $title ?? "Untitled Page" ?></h1>
+    </div>
+    
+    <div>
+        <?php foreach ($actions ?? [] as $action) { ?>
+        <a
+            class="text-sm px-3 py-2 rounded bg-blue-300 hover:bg-blue-400/70 active:bg-blue-400/90"
+            href="<?= $action[0] ?>"
+            ><?= $action[1] ?></a>
+        <?php } ?>
     </div>
     
 </section>
