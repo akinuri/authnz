e2754261caca5c49a8d2d1065751ad1722108077
Noreh AD <akinoreh@gmail.com>
2023-09-12 22:50:41 +0300

    dashboard stat cards

--- php/dashboard.php
+++ php/dashboard.php
@@ -12,6 +12,26 @@
     });
 ?>
 
-<div class="bg-white/25 rounded shadow h-96 p-4">
-    <span class="opacity-50">Dashboard</span>
+<div class="flex gap-4 text-lg">
+    
+    <div class="bg-white rounded shadow p-4 grow flex justify-between">
+        <span>Users</span>
+        <span>0</span>
+    </div>
+    
+    <div class="bg-white rounded shadow p-4 grow flex justify-between">
+        <span>Roles</span>
+        <span>0</span>
+    </div>
+    
+    <div class="bg-white rounded shadow p-4 grow flex justify-between">
+        <span>Permissions</span>
+        <span>0</span>
+    </div>
+    
+    <div class="bg-white rounded shadow p-4 grow flex justify-between">
+        <span>Settings</span>
+        <span>0</span>
+    </div>
+    
 </div>
